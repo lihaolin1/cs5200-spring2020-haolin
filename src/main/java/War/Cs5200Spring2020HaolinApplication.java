9f65cdd153370e1dff7af8df8f2edaa854bb2627
lihaolin1 <lihaolin.@husky.neu.edu>
2020-01-24 23:19:39 -0500

    first commit

---
+++ src/main/java/War/Cs5200Spring2020HaolinApplication.java
@@ -0,0 +1,13 @@
+package War;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Spring2020HaolinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Spring2020HaolinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/War/Cs5200Spring2020HaolinApplicationTests.java
@@ -0,0 +1,13 @@
+package War;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs5200Spring2020HaolinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
